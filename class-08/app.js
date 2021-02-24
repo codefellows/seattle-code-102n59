501dffe54714ea726619c9a3764b96ba657bcdbd
mnfmnfm <mnfmnfm@gmail.com>
2021-02-23 19:55:28 -0800

    day 8 demo

--- class-08/app.js
+++ class-08/app.js
@@ -6,3 +6,19 @@ document.write(paragraphizer(pictureType));
 function paragraphizer(pic) {
   return '<p>The premier taker of ' + pic + ' pictures.</p>';
 }
+
+function prettyPicture() {
+  return '<img class="profile" src="https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png" />';
+}
+
+// ask the user how many pictures they want to see
+let numPictures = prompt('How many photographers would you like to see?');
+// as long as they enter a bad response, keep prompting them
+while (isNaN(numPictures) || numPictures > 10 || numPictures < 1) {
+  numPictures = prompt('Try again. Please enter a number between 1 and 10.');
+}
+// use their response to show that many pictures
+for (let i = 0; i < numPictures; i = i + 1) {
+  document.write(prettyPicture());
+}
+
